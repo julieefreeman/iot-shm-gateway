200eeda2c6edcfad6c5b008ee7ba4613af0cc7e5
Douglas Sigelbaum <dsigelbaum3@gatech.edu>
2015-04-01 02:26:53 +0000

    updated python-xbee library

--- python-xbee/dist/XBee-2.1.0/examples/receive_samples.py
+++ python-xbee/dist/XBee-2.1.0/examples/receive_samples.py
@@ -10,7 +10,7 @@ This example continuously reads the serial port and processes IO data
 received from a remote XBee.
 """
 
-from xbee import XBee
+from xbee import ZigBee
 import serial
 
 PORT = '/dev/ttyUSB0'
@@ -20,11 +20,12 @@ BAUD_RATE = 9600
 ser = serial.Serial(PORT, BAUD_RATE)
 
 # Create API object
-xbee = XBee(ser)
+xbee = ZigBee(ser)
 
 # Continuously read and print packets
 while True:
     try:
+        print("about to read.")
         response = xbee.wait_read_frame()
         print(response)
     except KeyboardInterrupt:
